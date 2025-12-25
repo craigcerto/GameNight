e38a20d6d05e9e2f34ef8e6cb3a539ebb3e55ce8
Craig Certo <craigcerto@gmail.com>
2025-12-24 23:50:08 -0500

    upd

--- next.config.js
+++ next.config.js
@@ -10,20 +10,6 @@ const nextConfig = {
       },
     ],
   },
-
-  // Explicitly configure output file tracing
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64',
-        '.git/**/*',
-        'scripts/**/*',
-        'supabase/**/*',
-      ],
-    },
-  },
 }
 
 module.exports = nextConfig
